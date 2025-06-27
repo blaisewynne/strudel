fd041e62886327ee20dd6b31f95119e3750761fb
blaisewynne <164962110+blaisewynne@users.noreply.github.com>
2025-06-27 21:52:05 +1000

    Update songofhealing.js

--- songofhealing.js
+++ songofhealing.js
@@ -1,8 +1,8 @@
 // melody
 $: stack(
   `[B A F] [B A F] [B A E D E] [B A F] [B A F] 
-  [B A E D E] [F C B] [F C B] [F E B A B] [F C B] 
-  [F C B] [F E B G] [A A A A D D D] [G G G C G] 
+  [B A E D E] [F C Bb] [F C Bb] [F E B A B] [F C Bb] 
+  [F C Bb] [F E B G] [A A A D D D] [G G G C G] 
   [F F F Bb Bb Bb E D A E] [A A A D D D]  [G G G C G] 
   [F G C A Bb C D] [A B# D E]`)
-  .s('triangle').note().slow(8)
+  .s('triangle').note().slow(24)
